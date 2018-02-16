c315c1d4c0190e4d0c69389f2d092f3d76cce597
Benjamin <Redhaus@Benjamins-MacBook-Pro.local>
2018-02-15 20:18:02 -0800

    added UPDATED /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // import express and body parser as middleware
-var express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
 
 
 // import local mongoose db connect and data models
@@ -38,10 +39,6 @@ app.post('/todos', (req, res) => {
 
 })
 
-// point app to server location
-app.listen(port, () => {
-    console.log(`Started on port ${port}`)
-})
 
 
 app.get('/todos', (req, res) => {
@@ -101,6 +98,91 @@ app.delete('/todos/:id', (req, res) => {
 
 
 })
+
+
+// update items
+// app.patch('/todos/:id', (req, res) => {
+//     var id = req.params.id; 
+
+//     //create body var outlining what can be updated
+//     // pick function pics text and complted off req.body object
+//     var body = _.pick(req.body['text', 'completed']);
+
+//     if(!ObjectID.isValid(id)){ // check if id is valid
+//         return res.status(404).send('not valid id')
+//     }
+
+//     // check if completed is bool and if it is true set completed at
+//     // this is how we update completed at property
+//     if(_.isBoolean(body.completed) && body.completed){
+//         body.completedAt = new Date().getTime();
+//     } else { //else make it false and reset
+//         body.completed = false
+//         body.completedAt = null
+//     } 
+
+//     // update body of todo
+//     Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+//         .then( (todo) => {
+//             if(!todo){ // if no todo return 404
+//                 return res.status(404).send("Can't find ")
+//             }
+
+//             res.send({todo}) //return todo if there
+//             // res.send({todo:todo}) 
+            
+
+//         }).catch((e) => {
+//             res.status(404).send('No update')
+//         })
+
+// })
+
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+  
+    if(!ObjectID.isValid(id)){ // check if id is valid
+        return res.status(404).send('not valid id')
+    }
+  
+    // check if completed is bool and if it is true set completed at
+    // this is how we update completed at property
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else { //else make it false and reset
+        body.completed = false
+        body.completedAt = null
+    } 
+
+        // update body of todo
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then( (todo) => {
+            if(!todo){ // if no todo return 404
+                return res.status(404).send("Can't find ")
+            }
+
+            res.send({todo}) //return todo if there
+            // res.send({todo:todo}) 
+            
+
+        }).catch((e) => {
+            res.status(404).send('No update')
+        })
+
+
+  });
+
+
+
+
+// point app to server location
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
+})
+
+
 
 // valid id using is valid 
 // 404 if not
